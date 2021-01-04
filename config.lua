92ee6518860a71e4a4660bbff11395ff1ee79e54
Ruqen <73693641+Ruqen@users.noreply.github.com>
2021-01-04 17:23:37 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,7 @@
 Config = {}
 Config.WaitPressMinute = 30 --minute
 Config.RequiredItems = {
-    [1] = {label = 'Su',item = 'water'},
-    [2] = {label = 'Gübre',item = 'fertilizer'},
-    [3] = {label ='Esrar',item = 'weedq'},
-    [4] = {label = 'Esrar',item = 'weedq'},
-    [5] = {label = 'Su',item = 'water'}
+    [1] = {label = 'Water',item = 'water'},
+    [2] = {label = 'Fertilizer',item = 'fertilizer'},
+    [3] = {label ='Weed',item = 'weedq'},
 }
